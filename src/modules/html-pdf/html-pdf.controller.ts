a4e1535df51d1c1b36c1c627a07dd20627e5ad05
rg-rupeee <rupeshgarhwal3920@gmail.com>
2023-09-24 20:13:11 +0530

    catchAsync - error handler for controller level

--- src/modules/html-pdf/html-pdf.controller.ts
+++ src/modules/html-pdf/html-pdf.controller.ts
@@ -2,53 +2,48 @@
 import { NextFunction, Request, Response } from 'express';
 
 import HtmlToPdfService from './html-pdf.service';
+import catchAsync from '@middlewares/catchAsync.middleware';
 import { MulterRequest } from '@interfaces/request.interface';
 import { urlToPdfDTO, dataToPdfDTO, fileToPdfDTO } from './html-pdf.dto';
 
 class HtmlToPdfController {
   private service = new HtmlToPdfService();
 
-  public createPdfFromUrl = async (
-    req: Request,
-    res: Response,
-    _next: NextFunction,
-  ) => {
-    const { url, callbackUri } = req.body as urlToPdfDTO;
+  public createPdfFromUrl = catchAsync(
+    async (req: Request, res: Response, _next: NextFunction) => {
+      const { url, callbackUri } = req.body as urlToPdfDTO;
 
-    const data = await this.service.createPdfFromUrl(url, callbackUri);
+      const data = await this.service.createPdfFromUrl(url, callbackUri);
 
-    return res.status(201).json({ success: true, data });
-  };
+      return res.status(201).json({ success: true, data });
+    },
+  );
 
-  public createPDFFromData = async (
-    req: Request,
-    res: Response,
-    _next: NextFunction,
-  ) => {
-    const { fileData, callbackUri } = req.body as dataToPdfDTO;
+  public createPDFFromData = catchAsync(
+    async (req: Request, res: Response, _next: NextFunction) => {
+      const { fileData, callbackUri } = req.body as dataToPdfDTO;
 
-    const data = await this.service.createPDFFromData(fileData, callbackUri);
+      const data = await this.service.createPDFFromData(fileData, callbackUri);
 
-    return res.status(201).json({ success: true, data });
-  };
+      return res.status(201).json({ success: true, data });
+    },
+  );
 
-  public createPDFFromFile = async (
-    req: Request,
-    res: Response,
-    _next: NextFunction,
-  ) => {
-    const { file } = req as MulterRequest;
-    const { callbackUri } = req.body as fileToPdfDTO;
+  public createPDFFromFile = catchAsync(
+    async (req: Request, res: Response, _next: NextFunction) => {
+      const { file } = req as MulterRequest;
+      const { callbackUri } = req.body as fileToPdfDTO;
 
-    if (!file)
-      return res
-        .status(400)
-        .json({ success: false, message: 'please provide file' });
+      if (!file)
+        return res
+          .status(400)
+          .json({ success: false, message: 'please provide file' });
 
-    const data = await this.service.createPDFFromFile(file, callbackUri);
+      const data = await this.service.createPDFFromFile(file, callbackUri);
 
-    return res.status(201).json({ success: true, data });
-  };
+      return res.status(201).json({ success: true, data });
+    },
+  );
 }
 
 export default HtmlToPdfController;
